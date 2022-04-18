0f6ef8660a270735728d79f8f090b531d35405ca
im4kshay <akshaysayre7@gmail.com>
2022-04-18 19:39:08 +0530

    Handle Exception if User Provides Invalid Mood

--- MoodAnalyzerProblem/MoodAnalyzer.cs
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -19,19 +19,27 @@ namespace MoodAnalyzerProblem
         {
             this.message = message;
         }
-   
+
         ///UC1:-Analyse Happy And Sad Mood
         ///if messege Contain Sad then return Sad
         ///if message contain Happy then return Happy
-        
+
         public string AnalyzeMood()
         {
-            if (this.message.ToLower().Contains("sad")) //Contains():- This method is used to check whether the substring occurs within a given string or not.
-                return "sad";
-            else
+            try // Block of Code to be tested for error while it is being executed
+            {
+                if (this.message.Contains("sad"))//Contains():- This method is used to check whether the substring occurs within a given string or not.
+                    return "sad";
+                else if (this.message.Contains("happy"))
+                    return "happy";
+                else if (this.message.Contains(null))
+                    return "happy";
+            }
+            catch // Block of code to be Executed if an error in the block
+            {
                 return "happy";
-
-            //return null;
+            }
+            return null; // Value Return for all code path (AnalyzeMood)
         }
 
     }
